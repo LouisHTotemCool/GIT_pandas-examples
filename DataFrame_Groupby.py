8bfd3ec035be5c104ba0f846e8cc6178bbfe49a2
merouanedbt <merouane.debot1@gmail.com>
2025-04-07 13:46:22 +0200

    correction fonction sortRatingsField

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -61,6 +61,6 @@ print('\n==================================================================\n')
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
 sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
-    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
-print('My info sorted: \n%s' % sortRatingsField[:15])
+sortRatingsFieldV2 = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
+    COUNT='count', myAVG='mean').sort_values('COUNT', ascending=False)
+
